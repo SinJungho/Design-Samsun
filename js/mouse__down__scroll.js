1953c38ca46589b291fa3f1a77b847030cea1ba4
SinJungho <zmtls0516@naver.com>
2023-06-08 23:37:09 +0900

    cnt2 text add

---
+++ js/mouse__down__scroll.js
@@ -0,0 +1,36 @@
+var elements = document.querySelectorAll(".element");
+var currentIndex = 0;
+
+function showNextElement() {
+  if (currentIndex < elements.length) {
+    elements[currentIndex].classList.remove("hidden");
+    currentIndex++;
+  }
+}
+
+function deleteElement(params) {
+  if (currentIndex > 0) {
+    currentIndex--;
+    elements[currentIndex].classList.add("hidden");
+  }
+}
+
+function handleMouseWheel(event) {
+  var delta = Math.max(-1, Math.min(1, event.wheelDelta || -event.detail));
+  if (delta < 0) {
+    // Scroll down
+    showNextElement();
+  } else {
+    deleteElement();
+  }
+  // Prevent the default scroll behavior
+  event.preventDefault();
+}
+
+// Add the mouse wheel event listener to the document
+if (document.addEventListener) {
+  document.addEventListener("mousewheel", handleMouseWheel, false); // For modern browsers
+  document.addEventListener("DOMMouseScroll", handleMouseWheel, false); // For Firefox
+} else {
+  document.attachEvent("onmousewheel", handleMouseWheel); // For older IE versions
+}
